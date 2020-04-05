341ade8d2550d78a3ba4e6e199fe2d3b2f0dc5b1
Soutrik Chatterjee <chatterjee.soutrik@gmail.com>
2020-04-05 22:28:23 +0530

    Added ConnectionRefusedError handling and port and host input

--- telnet.py
+++ telnet.py
@@ -1,11 +1,16 @@
+# This program checks if a port is open in a specific host
+# Author : Soutrik Chatterjee
+
+
 import telnetlib
 import socket
 import os
 import subprocess
 
 new_socket = socket.socket()
-host = '192.168.0.11'
-port = 81
+
+host = input("Enter IP address of the machine ")
+port = int(input("Enter the port to check "))
 
 try :
 	s = new_socket.connect((host, port))
@@ -14,6 +19,10 @@ except InterruptedError :
 	print("Connection Interrupted")
 except TimeoutError :
 	print("Connection Timed out")
+except ConnectionRefusedError :
+	print("Connection refused by remote host")
 finally :
 	if new_socket.timeout :
 		print("Connection Failure")
+
+w = input("Press enter to exit") 
